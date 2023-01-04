729bf65164bba1794214e97e78a87f6ed98bdbeb
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2023-01-04 12:29:00 +0200

    first code

---
+++ main.js
@@ -0,0 +1,60 @@
+let input= document.querySelector(".input")
+let submit= document.querySelector(".supmit")
+let container_tasxs= document.querySelector(".container-tasxs")
+let arrayoftasks= [];
+
+
+   if(localStorage.getItem("tasks")){
+    arrayoftasks =+ JSON.Parse(localStorage.getItem("tasks"))
+}
+getDataFromLocalStorage()
+submit.onclick=function(){
+    if(input.value !=""){
+        addTaskToArray(input.value)
+        input.value =""
+    }
+    else{
+        alert("please enter  a task..")
+    }
+}
+function addTaskToArray(taskText){
+const task= {
+    id: Date.now(),
+    title:taskText,
+    completed:false,
+
+}
+arrayoftasks.push(task)
+addElementToPageFrom(arrayoftasks)
+adddatafromaeeaytoLocalStorage(arrayoftasks)
+
+}
+function addElementToPageFrom(arrayoftasks){
+    container_tasxs.innerHTML= ""
+   arrayoftasks.forEach(task => {
+    let div= document.createElement("div")
+    div.className= "task"
+    div.setAttribute("data-id" , task.id)
+    div.appendChild(document.createTextNode(task.title))
+    let span = document.createElement("span")
+    span.className= "span"
+    span.appendChild(document.createTextNode("Delete"))
+    div.appendChild(span)
+
+    container_tasxs.appendChild(div)
+    
+
+   });
+}
+ function adddatafromaeeaytoLocalStorage(arrayoftasks){
+  window.localStorage.setItem("tasks ",JSON.stringify(arrayoftasks))
+
+ }
+function getDataFromLocalStorage(){
+    let data = window.localStorage.getItem("tasks")
+    if(data){
+        let tasks= JSON.Parse(data)
+        console.log(tasks)
+        addElementToPageFrom(tasks)
+    }
+}
